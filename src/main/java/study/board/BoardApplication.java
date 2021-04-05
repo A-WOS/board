aee729ea58a75313e4bded66387bdd78f4c27eef
cgj10 <cgj1000@naver.com>
2021-04-05 15:39:47 +0900

    helloworld 내용 삭제

--- src/main/java/study/board/BoardApplication.java
+++ src/main/java/study/board/BoardApplication.java
@@ -8,7 +8,6 @@ public class BoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
-        System.out.println("hello gyujin!");
 
     }
 
