d1533e1126b9721d58ba900d5045cf1a5d3adaa6
Awos <uws0331@gmail.com>
2021-04-12 19:13:03 +0900

    게시판 작성 페이지 구현

--- src/main/java/study/board/controller/HelloController.java
+++ src/main/java/study/board/controller/HelloController.java
@@ -10,5 +10,7 @@ public class HelloController {
     public String hello() {
         return "hello";
 //        return "hello.html";
+
+        // devtools 라이브러리를 다운을 받고 ctrl+shift+f9를 누르면 코드 변경후 적용이 바로바로됨.
     }
 }
