9954fa0669d78e88abacbb3fd9f41fc0d7f66580
Awos <uws0331@gmail.com>
2021-04-12 19:22:41 +0900

    부트스트랩 미적용이어서 버전을 올려 적용

--- src/main/java/study/board/controller/HelloController.java
+++ src/main/java/study/board/controller/HelloController.java
@@ -6,11 +6,11 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
-    @GetMapping("/")
-    public String hello() {
-        return "hello";
-//        return "hello.html";
+//    @GetMapping("/")
+//    public String hello() {
+//        return "hello";
+////        return "hello.html";
 
         // devtools 라이브러리를 다운을 받고 ctrl+shift+f9를 누르면 코드 변경후 적용이 바로바로됨.
-    }
+//    }
 }
