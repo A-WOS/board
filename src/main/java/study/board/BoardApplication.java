578edeb2046c2b27e1b61f0bc01b64aec9621011
Awos <uws0331@gmail.com>
2021-05-27 22:17:00 +0900

    글 수정, 삭제 잘되고 인덱스 1씩 올라감

--- src/main/java/study/board/BoardApplication.java
+++ src/main/java/study/board/BoardApplication.java
@@ -2,7 +2,9 @@ package study.board;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 
 /* JPA Auditing 기능을 사용하기 위해 main 클래스(BoardApplication)에 @EnableJpaAuditing 어노테이션을 붙여줍니다. */
 @EnableJpaAuditing
@@ -11,5 +13,10 @@ public class BoardApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(BoardApplication.class, args);
+    }
+
+    @Bean
+    public HiddenHttpMethodFilter hiddenHttpMethodFilter() {
+        return new HiddenHttpMethodFilter();
     }
 }
